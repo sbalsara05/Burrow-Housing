72b9fce689a9f22ee1efb113c9ef95a0069b8c7f
sbalsara05 <balsarasohum@gmail.com>
2024-11-09 11:51:26 -0500

    Updated Hero Banner and added new pictures of northeastern

--- themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/src/components/homes/home-three/HeroBanner.tsx
+++ themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/src/components/homes/home-three/HeroBanner.tsx
@@ -16,9 +16,9 @@ const HeroBanner = () => {
    return (
       <div className="hero-banner-three position-relative z-1 pt-130 lg-pt-100 pb-170 xl-pb-130 lg-pb-100">
          <Slider {...setting} className="hero-slider-one m0">
-            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/img_26.jpg)` }}></div></div>
-            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/img_27.jpg)` }}></div></div>
-            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/img_28.jpg)` }}></div></div>
+            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/NEU-ISEC.jpg)` }}></div></div>
+            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/NEU-QUAD-2.png)` }}></div></div>
+            <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/NEU-WIDE-SHOT.jpg)` }}></div></div>
          </Slider>
 
          <div className="container position-relative z-2">
