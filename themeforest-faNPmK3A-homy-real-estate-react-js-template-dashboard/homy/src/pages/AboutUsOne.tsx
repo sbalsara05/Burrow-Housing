1b259debdd3a52cecbfc54a4d2cfb595cd05c8f0
jbmus <jbmuslu@hotmail.com>
2024-11-19 12:28:58 -0500

    got rid of unecesary stuff on about page and added own details

--- themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/src/pages/AboutUsOne.tsx
+++ themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/src/pages/AboutUsOne.tsx
@@ -5,7 +5,7 @@ import AboutUsOneMain from '../components/inner-pages/about-us/about-us-one';
 const AboutUsOne = () => {
    return (
       <Wrapper>
-         <SEO pageTitle={'About Us One Homy'} />
+         <SEO pageTitle={'About Us'} />
          <AboutUsOneMain />
       </Wrapper>
    );
