63cbbc10fa1546d5b51115b4d5f7ff54cdd9b86e
sbalsara05 <balsarasohum@gmail.com>
2024-11-20 09:34:54 -0500

    Updated User Schema

--- themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/backend/UserSchema.js
+++ themeforest-faNPmK3A-homy-real-estate-react-js-template-dashboard/homy/backend/UserSchema.js
@@ -24,8 +24,6 @@ const UserSchema = new mongoose.Schema(
         },
         about: String,
         avatar: { type: String, default: 'default-avatar.png' },
-        smoker: { type: Boolean, default: false },
-        allergies: [String],
         password: { type: String, required: true },
         role: { type: String, enum: ['user', 'admin'], default: 'user' },
     },
