46d7f14bd861badfd0a6aebe25fe05df89bb73bb
Hardi-Shah-002301430 <shah.hardim@northeastern.edu>
2025-07-15 21:29:10 -0400

    deleting unused media and change agent to about_us

--- homy/src/data/home-data/AboutUsData.ts
+++ homy/src/data/home-data/AboutUsData.ts
@@ -6,35 +6,35 @@ interface DataType {
    desc: string;
 }
 
-const agent_data: DataType[] = [
+const about_us_data: DataType[] = [
    {
       id: 1,
       page: "home_1",
-      thumb: "/assets/images/agent/sohum_01.jpg",
+      thumb: "/assets/images/about_us/sohum_01.jpg",
       title: "Sohum Balsara",
       desc: "CEO & Founder",
    },
    {
       id: 2,
       page: "home_1",
-      thumb: "/assets/images/agent/hardi_03.jpg",
+      thumb: "/assets/images/about_us/hardi_03.jpg",
       title: "Hardi Shah",
       desc: "CTO & Co-Founder",
    },
    {
       id: 3,
       page: "home_1",
-      thumb: "/assets/images/agent/aarnav_02.png",
+      thumb: "/assets/images/about_us/aarnav_02.png",
       title: "Arnav Sawant",
       desc: "CBO & Co-founder",
    },
    {
       id: 4,
       page: "home_1",
-      thumb: "/assets/images/agent/sujay_04.jpeg",
+      thumb: "/assets/images/about_us/sujay_04.jpeg",
       title: "Sujay Sanakka Nagarajappa",
       desc: "Director of Engineering",
    },
 ]
 
-export default agent_data;
+export default about_us_data;
